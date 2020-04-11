e2de359ce9d5ef9fc66f57d3414c5a9ed715d0d1
Vicentelihn <vlihn@miuandes.cl>
2020-04-11 12:50:23 -0400

    VicenteLihn

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -12,10 +12,12 @@ class Ticket < ApplicationRecord
       es = self.ticket_type.event.event_stat
       ev = self.ticket_type.event.event_venue
       if es.attendance >= ev.capacity
-        puts 'Error! the amount of tickets sold for an event surpasses the capacity of the corresponding event venue.'
+        raise StandardError.new "Error! the amount of tickets sold for an event surpasses the capacity of the corresponding event venue."
       else
         es.tickets_sold+=1
         es.attendance+=1
+        es.save
+        #al poner es.save! me sale "cannot find save!" nose si importe, pero lo comento porsiacaso
       end
     end
   end
@@ -24,4 +26,8 @@ class Ticket < ApplicationRecord
       es = self.ticket_type.event.event_stat
       es.tickets_sold-=1
       es.attendance-=1
+      es.save
     end
+  #end
+#end
+#al igual que mas arriba, lo comento porsiacaso, al poner los ultimo 2 "end" se me marcan en rojo y dice "Unexpected end"
